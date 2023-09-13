a64ee3e018501c5c948b2c7baa1b7c46f7214b7b
moyukai05 <moyukai05@kuaishou.com>
2023-09-13 16:26:43 +0800

    save

--- src/good-cards/s0/runtime.tsx
+++ src/good-cards/s0/runtime.tsx
@@ -1,65 +1,87 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import TitleWithIcon from '../title-with-icon';
 import styles from './styles.less';
-import { GoodModel, ICommodity } from '../constant';
+import { GoodModel, ICommodity } from './../constant';
 import { Price } from '../price';
 
-export default function ({ inputs, env }) {
-  const [model, setModel] = useState<GoodModel>({
-    commodity: {} as ICommodity,
-    index: 0,
-    onPress: () => {},
-    onExpose: () => {},
-  });
-  const { commodity, index, onPress } = model;
+export default function ({ data, inputs, env }) {
+  const [model, setModel] = useState();
+  const topTags = [
+    'https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/compress/image-6e871feb-e1eb-46c7-9567-253abaf1d181.png',
+    'https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/compress/image-8c8841d2-667b-4674-8dc3-ce198c572902.png',
+    'https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/compress/image-39f99eb8-b9e6-464d-91ca-d4d4e52eb6e4.png',
+  ];
+
+  const renderTagList = (itemTagList: string[] = []) => {
+    let sum = 0,
+      total = 15;
+
+    return (
+      <div className={styles.goodTagList}>
+        {itemTagList?.map((tag, index) => {
+          sum += tag.length;
+          if (sum > total) {
+            return null;
+          }
+          return (
+            <>
+              {index !== 0 ? <div className={styles.divider}></div> : null}
+              <span className={styles.goodTag}>{tag}</span>
+            </>
+          );
+        })}
+      </div>
+    );
+  };
 
   useEffect(() => {
-    inputs['install']((model: GoodModel) => {
+    inputs['install']((model) => {
+      console.log('安装时的model', model);
       setModel(model);
-      model.onExpose(model.commodity, model.index);
     });
   }, []);
 
-  const showOriginalPrice =
-    commodity?.itemOriginalPriceDoc && commodity?.itemOriginalPriceDoc != commodity?.itemCouponPriceDoc;
-
   return (
-    <div key={`commodity-${index}`} className={styles.card} onClick={() => onPress(commodity, model.index)}>
-      <img className={styles.commodityImg} data-src={commodity.itemImage} />
-      <div className={styles.commodityDetail}>
-        <div className={styles.title}>
-          {commodity.itemIcons?.map((item, index) => {
-            return (
-              <img
-                className={styles.nameTag}
-                style={{
-                  height: 14,
-                  width: 'auto',
-                }}
-                src={item}
-                key={item}
-              />
-            );
-          })}
-          {commodity.itemName}
+    <>
+      <div className={styles.goodCard}>
+        <div className={styles.coverWrapper}>
+          <img style={{ objectFit: 'cover' }} className={styles.cover} data-src={model.itemImage} />
+          {model.rank < 3 ? (
+            <img className={styles.top3Tag} src={topTags[model.rank]} />
+          ) : (
+            <div className={styles.otherTag}>
+              <span className={styles.otherTagText}>{model.rank + 1}</span>
+            </div>
+          )}
         </div>
-        <div className={styles.tagList}>
-          {commodity.itemTagList?.map((tag) => (
-            <div className={styles.goodTag}>{tag}</div>
-          ))}
+        <div className={styles.content}>
+          <div>
+            <TitleWithIcon
+              key={model.rank}
+              numberOfLines={2}
+              icons={model.itemIcons || []}
+              iconStyle={{ marginRight: '3px' }}
+              env={env}
+            >
+              {model?.itemName}
+            </TitleWithIcon>
+            {renderTagList(model?.itemTagList)}
+          </div>
+          <div className={styles.buyWrapper} style={{ backgroundImage: `url(${data.buyBg})` }}>
+            <div className={styles.priceWrapper}>
+              <div className={styles.price}>
+                <span className={styles.itemCouponPrice}>
+                  <Price>{model?.itemCouponPrice / 100}</Price>
+                </span>
+                {model?.itemCouponPrice !== model?.itemOriginalPrice ? (
+                  <span className={styles.itemOriginalPrice}>￥{model?.itemOriginalPrice / 100}</span>
+                ) : null}
+              </div>
+            </div>
+            <div className={styles.buyBtn}>去抢购</div>
+          </div>
         </div>
-        <div className={styles.flex1}></div>
-        <div className={styles.priceBlock}>
-          <span className={styles.prefix}>{commodity.itemCouponPriceTitle}</span>
-          <Price className={styles.couponPrice}>
-            {commodity?.itemCouponPriceDoc}
-          </Price>
-          {showOriginalPrice ? (
-            <span className={styles.originalPrice}>¥{commodity?.itemOriginalPriceDoc}</span>
-          ) : null}
-        </div>
-        <div className={styles.buyButton}>去抢购</div>
       </div>
-    </div>
+    </>
   );
 }
