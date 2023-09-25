0ce3c09a0b34e2e0a3dbb7c17fa306e50b1f5de0
moyukai05 <moyukai05@kuaishou.com>
2023-09-25 12:04:25 +0800

    save

---
+++ src/CMB/components/t3-shop-card/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div :class="css.shopList">
+            <div v-for="item in 6" :key="item" :class="css.shopItem">
+                <img :class="css.shopImage" src="http://my.mybricks.world/mfs/files/1695612961358/JRG7o0rmOGh5FxC3QaRK4KmpdO8kr9CL-1695612962527.png" alt="商品图片">
+                <div :class="css.shopTitle">【咖啡馆】星巴克星选咖啡拿铁270ml</div>
+                <div :class="css.shopPrice">20元</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+ 
+<script>
+import css from "./index.less";
+
+export default {
+    name: 'T3ShopCard',
+    props: {
+    },
+    data() {
+        return {
+            css: css
+
+        };
+    }
+}
+</script>
+
+<style scoped lang="less"></style>
