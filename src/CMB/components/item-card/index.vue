667338f5de001ff43b0c9502180c0ecf2eee2804
moyukai05 <moyukai05@kuaishou.com>
2023-09-25 10:56:00 +0800

    save

--- src/CMB/components/item-card/index.vue
+++ src/CMB/components/item-card/index.vue
@@ -5,20 +5,25 @@
                 src="http://my.mybricks.world/mfs/files/1695561706060/FyQHBHJ3gRvOGA9EZCX5fpbs9dfiO9qg-1695561706303.png"
                 alt="">
             <div :class="css.padding_top"></div>
-            <div :class="css.content">
-                <!-- <slot id="contant"></slot> -->
-                <div :class="css.pic">
-                    <img src="http://my.mybricks.world/mfs/files/1695573956590/ZhmDovdubs8HAOOlyrojTDIs0LaNaHHW-1695573956802.png"
-                        alt="">
-                </div>
+            <div :class="css.content" style="width: 375px;">
+                <slot></slot>
+ 
             </div>
         </div>
     </div>
 </template>
 
-<script setup>
+<script>
 import css from "./index.less";
 
+export default {
+    name: 'ItemCard',
+    data() {
+        return {
+            css: css
+        };
+    }
+}
 </script>
 
 <style scoped lang="less"></style>
--- src/CMB/item-card/runtime.vue
+++ src/CMB/item-card/runtime.vue
@@ -1,21 +1,37 @@
 <template>
     <div>
-        <div :class="css.card">
-            <img :class="css.title" src="http://my.mybricks.world/mfs/files/1695561706060/FyQHBHJ3gRvOGA9EZCX5fpbs9dfiO9qg-1695561706303.png" alt="">
-            <div :class="css.padding_top"></div>
-            <div :class="css.content">
-                <!-- <slot id="contant"></slot> -->
-                <div :class="css.pic">
-                    <img src="http://my.mybricks.world/mfs/files/1695573956590/ZhmDovdubs8HAOOlyrojTDIs0LaNaHHW-1695573956802.png" alt="">
-                </div>
-            </div>
-        </div>
+        <ItemCard>
+            <!-- 在组件里面引入css.样式/或者直接使用下方样式引入会报错/不生效 -->
+            <img style="width:95%;margin: 0 auto;border-radius: 10px;border: #F8EED7 2px solid;" class="pic"
+                src="http://my.mybricks.world/mfs/files/1695573956590/ZhmDovdubs8HAOOlyrojTDIs0LaNaHHW-1695573956802.png"
+                alt="">
+        </ItemCard>
     </div>
 </template>
 
-<script setup>
+<script>
 import css from "./index.less";
+import ItemCard from "../components/item-card/index.vue";
+
+export default {
+    components: {
+        ItemCard
+    },
+    data() {
+        return {
+
+        }
+    }
+};
 
 </script>
 
-<style scoped lang="less"></style>
+<style lang="less">
+// .pic {
+//     width: 200px;
+//     display: flex;
+//     justify-content: center;
+//     border: #F8EED7 2px solid;
+//     border-radius: 10px;
+// }
+</style>
