83fc075f1a2d74614d602a5416cd0bc8728cb66f
moyukai05 <moyukai05@kuaishou.com>
2023-09-25 15:26:18 +0800

    save

---
+++ src/CMB-ZL/shop-card/runtime.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <ItemCard bgColor="black"
+            headerSrc="http://my.mybricks.world/mfs/files/1695623624389/AisY26zJAxvA8xxg87rHNDscyCj1IsDZ-1695623625003.png">
+            <T2ShopCard>
+
+                
+            </T2ShopCard>
+        </ItemCard>
+    </div>
+</template>
+
+<script>
+import css from "./index.less";
+import ItemCard from "../components/item-card/index.vue";
+import T2ShopCard from "../components/t2-shop-card/index.vue";
+
+export default {
+    components: {
+        ItemCard,
+        T2ShopCard
+    },
+    data() {
+        return {
+
+        }
+    }
+};
+
+</script>
+
+<style lang="less">
+// .pic {
+//     width: 200px;
+//     display: flex;
+//     justify-content: center;
+//     border: #F8EED7 2px solid;
+//     border-radius: 10px;
+// }
+</style>
