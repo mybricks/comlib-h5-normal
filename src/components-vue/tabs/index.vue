dc0ce57137d780475ef4de6ceb6439716e99282b
moyukai05 <moyukai05@kuaishou.com>
2023-09-26 20:46:50 +0800

    save:vue tab组件支持样式编辑

--- src/components-vue/tabs/index.vue
+++ src/components-vue/tabs/index.vue
@@ -5,7 +5,7 @@
                 <div v-for="item in items" :key="item._id" @click="tabClick(item._id)"
                     :class="['tabItem_line',isActive(item._id)]" style="flex-grow: 0;">
                     {{ item.tabName }}
-                    <div v-if="item._id === value" class="line" style="background-color: #FA6400"></div>
+                    <div v-if="item._id === value" class="line taroify-tabs__line"></div>
                 </div>
             </div>
         </div>
@@ -37,7 +37,7 @@ export default {
             this.$emit('change', id)
         },
         isActive(itemId){
-            return itemId === this.value ? 'active' : '';
+            return itemId === this.value ? 'active taroify-tabs__tab--active' : 'taroify-tabs__tab';
         }
     }
 }
