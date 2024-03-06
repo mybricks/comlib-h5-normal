9004c680d9b3ebf474bd9e2e9d94852cadf356ad
zhaoxing03 <zhaoxing03@kuaishou.com>
2024-03-06 21:08:56 +0800

    beta

--- src/_connector/runtime.ts
+++ src/_connector/runtime.ts
@@ -1,46 +1,57 @@
-function callCon({env, data, inputs, outputs, onError}, params = {}) {
-
-  inputs["setGlobalHeaders"]((e) => {
-
-  });
-
-
-  inputs["setGlobalParams"]((e) => {
-
-  });
-
-
-  inputs["setGlobalBody"]((e) => {
-
-  });
-
-
+function callCon({ env, data, inputs, outputs, onError }, params = {}) {
   if (data.connector) {
     try {
-      env.callConnector(data.connector, params, data.connectorConfig).then(val => {
-        outputs['then'](val)
-      }).catch(err => {
-        outputs['catch'](err)
-      })
+      env
+        .callConnector(data.connector, params, data.connectorConfig)
+        .then((val) => {
+          outputs["then"](val);
+        })
+        .catch((err) => {
+          outputs["catch"](err);
+        });
     } catch (ex) {
-      console.error(ex)
+      console.error(ex);
 
-      outputs['catch'](`执行错误 ${ex.message||ex}`)
+      outputs["catch"](`执行错误 ${ex.message || ex}`);
       //onError(ex.message)
     }
   } else {
-    outputs['catch'](`没有选择接口`)
+    outputs["catch"](`没有选择接口`);
   }
 }
 
-export default function ({env, data, inputs, outputs, onError}) {
+export default function ({ env, data, inputs, outputs, onError }) {
   if (env.runtime) {
+    // //
+    // inputs["setGlobalHeaders"]((e) => {
+    //   if (typeof obj === "object" && obj !== null && !Array.isArray(obj)) {
+    //     Taro.setStorageSync("_MYBRICKS_GLOBAL_HEADERS_", obj);
+    //   }
+    //   return;
+    // });
+
+    // //
+    // inputs["setGlobalParams"]((e) => {
+    //   if (typeof obj === "object" && obj !== null && !Array.isArray(obj)) {
+    //     Taro.setStorageSync("_MYBRICKS_GLOBAL_PARAMS_", obj);
+    //   }
+    //   return;
+    // });
+
+    // //
+    // inputs["setGlobalBody"]((e) => {
+    //   if (typeof obj === "object" && obj !== null && !Array.isArray(obj)) {
+    //     Taro.setStorageSync("_MYBRICKS_GLOBAL_BODY_", obj);
+    //   }
+    //   return;
+    // });
+
     if (data.immediate) {
-      callCon({env, data, outputs})
+      callCon({ env, data, outputs });
     } else {
-      inputs['call'](params => {
-        callCon({env, data, outputs, onError}, params)
-      })
+      inputs["call"]((params) => {
+        callCon({ env, data, outputs, onError }, params);
+      });
     }
   }
 }
