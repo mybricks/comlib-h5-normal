dcc864a0da123507697fb3f8a99a1fc0d817ac5d
zhaoxing03 <zhaoxing03@kuaishou.com>
2024-03-12 12:02:20 +0800

    beta

--- src/containerCondition/editors.tsx
+++ src/containerCondition/editors.tsx
@@ -20,6 +20,7 @@ export default {
   "@resize": {
     options: ["width", "height"],
   },
+  ":slot": {},
   ":root"({ data, output, input, style, slots }, cate0, cate1, cate2) {
     cate0.title = "常规";
     cate0.items = [
