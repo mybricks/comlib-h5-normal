d2fed130848609e25aaf66a26c72f7c2daba221d
moyukai05 <moyukai05@kuaishou.com>
2024-03-20 11:37:55 +0800

    save

--- src/_if-empty/Tips/index.tsx
+++ src/_if-empty/Tips/index.tsx
@@ -3,11 +3,8 @@ import React, { FC, useCallback, useEffect, useState } from "react";
 import styles from "./index.less";
 
 const Tips: FC = () => {
-
   return (
     <div className={styles.tips}>
-      提示：
-      <br />
       通过判断上面输入的字段是否为空，决定输出到「非空」或者「空」输出，两种输出都会原样携带输入的数据；
       <br /> 判断的字段应该精确到某个值，而不是对象或数组;
       <br />
@@ -16,8 +13,19 @@ const Tips: FC = () => {
       <br />
       - 0
       <br />
-      - “”
-      <br />- undefined
+      - "0"
+      <br />
+      - ""
+      <br />
+      - undefined
+      <br />
+      - null
+      <br />
+      - false
+      <br />
+      - NaN
+      <br />- {"{}"}
+      <br />- []
     </div>
   );
 };
--- src/_if-empty/runtime.ts
+++ src/_if-empty/runtime.ts
@@ -7,15 +7,24 @@ export default function ({
   onError,
 }: RuntimeParams<Data>) {
   const { judged_field } = data;
-  inputs["inputContext"]((context:any) => {
+  inputs["inputContext"]((context: any) => {
     const sandbox: ExpressionSandbox = new ExpressionSandbox({
       context,
       prefix: "inputValue",
     });
 
-    try{
+    try {
       const ret = sandbox.executeWithTemplate(judged_field);
-      if(ret === null || ret === undefined || ret === "" || ret === 0){
+      if (
+        Number.isNaN(ret) ||
+        ret === null ||
+        ret === undefined ||
+        ret === "" ||
+        ret === 0 ||
+        ret === false ||
+        (Array.isArray(ret) && ret.length === 0) ||
+        (typeof ret === "object" && Object.keys(ret).length === 0)
+      ) {
         outputs["isEmpty"](context);
       } else {
         outputs["notEmpty"](context);
