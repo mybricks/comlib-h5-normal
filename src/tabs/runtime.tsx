63febb85ad4e74699d95f0a1ffe3bb0f8a61888b
moyukai05 <moyukai05@kuaishou.com>
2024-03-27 11:44:48 +0800

    fix:tab scope slot切换时不触发问题&新增tab吸顶（开发中）

--- src/tabs/runtime.tsx
+++ src/tabs/runtime.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useCallback, useEffect, useMemo } from "react";
 import { View } from "@tarojs/components";
+import * as Taro from "@tarojs/taro";
 import { Tabs } from "brickd-mobile";
 import css from "./style.less";
 
@@ -10,7 +11,96 @@ function getDefaultCurrTabId(tabs) {
   return "";
 }
 
+function getTabId(length) {
+  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var result = "";
+  for (var i = 0; i < length; i++) {
+    var randomPos = Math.floor(Math.random() * chars.length);
+    result += chars.substring(randomPos, randomPos + 1);
+  }
+  return "tabs-" + result;
+}
+
 export default function ({ data, inputs, outputs, title, slots, env }) {
+  const [isFixed, setIsFixed] = useState(false);
+  const [tabsTop, setTabsTop] = useState(0);
+  const [tabsTopReady, setTabsTopReady] = useState(false);
+  const [tabsHeight, setTabsHeight] = useState(0);
+  const [showPlaceholder, setShowPlaceholder] = useState(false);
+  const [TabID, setTabID] = useState(getTabId(6));
+
+  //判断是否是真机运行态
+  const isRelEnv = () => {
+    if (env.runtime.debug || env.edit) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  // 真机运行时获取tab的高度和距离顶部的距离
+  useEffect(() => {
+    if (isRelEnv()) {
+      const query = Taro.createSelectorQuery();
+      query
+        .select(`#${TabID}`)
+        .boundingClientRect()
+        .exec((res) => {
+          const rect = res[0];
+          console.log("rect", rect);
+          if (rect) {
+            setTabsTop(rect.top);
+            setTabsHeight(rect.height);
+            setTabsTopReady(true);
+          }
+        });
+    }
+  }, []);
+
+
+  useEffect(() => {
+    inputs["dataSource"]?.((ds) => {
+      if (Array.isArray(ds)) {
+        data.tabs = ds.map((item) => {
+          return {
+            _id: item.id,
+            tabName: item.tabName,
+          };
+        });
+      }
+    });
+
+    inputs["activeTabId"]?.((tabId) => {
+      if (tabId !== undefined || tabId !== null) {
+        _setCurrentTabId(tabId);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!tabsTopReady) return;
+    env?.rootScroll?.onScroll?.((e) => {
+      if (!data.sticky) return;
+      const { scrollTop } = e.detail ?? {};
+      //tabs初始化时已经在屏幕顶部了
+      if (tabsTop === 0) {
+        // console.log("设置为吸顶", TabID, scrollTop, tabsTop);
+        setShowPlaceholder(true);
+        setIsFixed(true);
+        return;
+      }
+      if (scrollTop >= tabsTop) {
+        // console.log("设置为吸顶", TabID, scrollTop, tabsTop);
+        setIsFixed(true);
+        setShowPlaceholder(true);
+      } else {
+        // console.log("设置为不吸顶", TabID, scrollTop, tabsTop);
+        setIsFixed(false);
+        setShowPlaceholder(false);
+      }
+    });
+  }, [tabsTop, tabsTopReady]);
+
   // 当前选中的tab
   const [currentTabId, setCurrentTabId] = useState(
     getDefaultCurrTabId(data.tabs)
@@ -32,7 +122,8 @@ export default function ({ data, inputs, outputs, title, slots, env }) {
   //   }
   // }, []);
 
-  const _setCurrentTabId = useCallback((currentTabId) => {
+  //点击tab进行切换
+  const _setCurrentTabId = (currentTabId) => {
     setCurrentTabId(currentTabId);
     const index = data.tabs.findIndex((tab) => tab._id == currentTabId);
     if (index === -1) {
@@ -44,26 +135,13 @@ export default function ({ data, inputs, outputs, title, slots, env }) {
       title: findItem.tabName,
       index,
     });
-  }, []);
 
-  useEffect(() => {
-    inputs["dataSource"]?.((ds) => {
-      if (Array.isArray(ds)) {
-        data.tabs = ds.map(item => {
-          return {
-            _id: item.id,
-            tabName: item.tabName,
-          }
-        });
-      }
-    });
-
-    inputs["activeTabId"]?.((tabId) => {
-      if (tabId !== undefined || tabId !== null) {
-        _setCurrentTabId(tabId);
-      }
-    });
-  }, []);
+    if (isRelEnv() && isFixed) {
+      //默认在顶部，切换后先滑动到页面顶部
+      env.rootScroll.scrollTo({ scrollTop: tabsTop, duration: 0 });
+      console.log("触发滚动复位,scrollTop", TabID, tabsTop);
+    }
+  };
 
   // 编辑模式下，切换tab
   // useEffect(() => {
@@ -82,40 +160,57 @@ export default function ({ data, inputs, outputs, title, slots, env }) {
 
   return (
     emptyView || (
-      <Tabs
-        className={css.tabs}
-        value={currentTabId}
-        onChange={_setCurrentTabId}
-        swipeable={data.swipeable}
-      >
-        {data.tabs.map((tab, index) => {
-          // let style = {};
-          // if (tab.useIndependentStyle) {
-          //   style = tab._id == currentTabId ? tab.activeNavItemStyle : tab.normalNavItemStyle;
-          // } else {
-          //   style = tab._id == currentTabId ? data.activeNavItemStyle : data.normalNavItemStyle;
-          // }
+      <View>
+        {showPlaceholder && <View style={{ height: `${tabsHeight}px` }}></View>}
+        <Tabs
+          id={TabID}
+          className={isFixed ? css.fix_tabs : css.tabs}
+          value={currentTabId}
+          onChange={_setCurrentTabId}
+          swipeable={data.swipeable}
+        >
+          {data.tabs.map((tab, index) => {
+            // let style = {};
+            // if (tab.useIndependentStyle) {
+            //   style = tab._id == currentTabId ? tab.activeNavItemStyle : tab.normalNavItemStyle;
+            // } else {
+            //   style = tab._id == currentTabId ? data.activeNavItemStyle : data.normalNavItemStyle;
+            // }
+            return (
+              <Tabs.TabPane
+                id="tabpane"
+                // style={{ ...style }}
+                key={tab._id}
+                title={tab.tabName}
+                value={tab._id}
+              ></Tabs.TabPane>
+            );
+          })}
+        </Tabs>
 
-          return (
-            <Tabs.TabPane
-              // style={{ ...style }}
-              key={tab._id}
-              title={tab.tabName}
-              value={tab._id}
-            >
-              {data.hideContent
-                ? null
-                : slots[data.tabs_dynamic ? "item" : tab._id].render?.({
-                    inputValues: {
-                      itemData: tab,
-                      index: index,
-                    },
-                    key: tab._id,
-                  })}
-            </Tabs.TabPane>
-          );
+        {data.tabs.map((tab, index) => {
+          if (currentTabId === tab._id) {
+            return (
+              <View
+                key={tab._id}
+                // style={{
+                //   display: currentTabId === tab._id ? "block" : "none",
+                // }}
+              >
+                {data.hideContent
+                  ? null
+                  : slots[data.tabs_dynamic ? "item" : tab._id].render?.({
+                      inputValues: {
+                        itemData: tab,
+                        index: index,
+                      },
+                      key: tab._id,
+                    })}
+              </View>
+            );
+          } 
         })}
-      </Tabs>
+      </View>
     )
   );
 }
