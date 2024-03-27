33f6f6d6fa48749701cf93395fcca3f945b747d4
liubiao03 <liubiao03@kuaishou.com>
2024-03-27 16:50:10 +0800

    feat: update

--- src/calendar/editors.tsx
+++ src/calendar/editors.tsx
@@ -59,6 +59,13 @@ export default {
                 outputId: "onSelect",
               },
             },
+            {
+              title: "当选择完毕时",
+              type: "_event",
+              options: {
+                outputId: "onConfirm",
+              },
+            },
           ],
         },
       ];
