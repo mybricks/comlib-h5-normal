38d820e3c6462b15150d730a630ed69fb64bd750
zhaoxing03 <zhaoxing03@kuaishou.com>
2024-07-10 15:36:17 +0800

    beta

--- src/cascader/runtime.tsx
+++ src/cascader/runtime.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useMemo, useState } from "react";
 import { Cascader } from "brickd-mobile";
 import mockData from "./mockData";
-
+import css from "./style.less";
 
 export default function ({ env, data, inputs, outputs, slots, mockProps }) {
   const [value, setValue] = useState<string[]>(
@@ -12,28 +12,28 @@ export default function ({ env, data, inputs, outputs, slots, mockProps }) {
   );
 
   useMemo(() => {
-    inputs['addDataSource']?.((val) => {
+    inputs["addDataSource"]?.((val) => {
       if (Array.isArray(val)) {
-        setOptions(val)
+        setOptions(val);
       }
     });
 
-    inputs['setValue']?.((val) => {
-      setValue(val)
+    inputs["setValue"]?.((val) => {
+      setValue(val);
     });
-  }, [])
+  }, []);
 
   const handleChange = useCallback((values, options) => {
-    outputs["onChange"]?.(options.filter(t => !!t));
-  }, [])
+    outputs["onChange"]?.(options.filter((t) => !!t));
+  }, []);
   const handleSelect = useCallback((values, options) => {
-    setValue(values)
-    outputs["onSelect"]?.(options.filter(t => !!t));
-  }, [])
-  
+    setValue(values);
+    outputs["onSelect"]?.(options.filter((t) => !!t));
+  }, []);
+
   return (
     <Cascader
-      className="mybricks-cascader"
+      className={css["mybricks-cascader"]}
       options={options}
       placeholder={data.placeholder}
       value={value}
