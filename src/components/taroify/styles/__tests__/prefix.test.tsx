ce0ace02c997d613581f12505caee7026a5b9bec
moyukai05 <moyukai05@kuaishou.com>
2024-10-11 21:15:54 +0800

    save

--- src/components/taroify/styles/__tests__/prefix.test.tsx
+++
@@ -1,9 +0,0 @@
-import { prefixClassname } from "../prefix"
-
-const __PREFIX = "taroify-"
-
-describe("Prefix classname", () => {
-  it("should be taroify-test", () => {
-    expect(prefixClassname("test")).toBe(`${__PREFIX}test`)
-  })
-})
