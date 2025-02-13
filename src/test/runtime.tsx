b8b3f1ada6a0c74945de70051f7901988d1204ab
zhaoxing03 <zhaoxing03@kuaishou.com>
2025-02-13 11:29:56 +0800

    beta

--- src/test/runtime.tsx
+++
@@ -1,6 +0,0 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
-import { View } from "@tarojs/components";
-
-export default function ({ env, data, inputs, outputs }) {
-  return <View>test</View>;
-}
