157f166b6200f07f97ad41c4f4502a1e37158df6
zhaoxing03 <zhaoxing03@kuaishou.com>
2025-02-26 14:21:41 +0800

    beta

--- src/table/edit/runtime.tsx
+++ src/table/edit/runtime.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 import Runtime from "./../runtime";
-import { useDisabledArea } from "./useDisabledArea";
+// import { useDisabledArea } from "./useDisabledArea";
 
 export default (props) => {
-  const DisabledArea = useDisabledArea();
+  // const DisabledArea = useDisabledArea();
 
-  return (
-    <DisabledArea>
-      <Runtime {...props} />
-    </DisabledArea>
-  );
+  return <Runtime {...props} />;
+  // return (
+  //   <DisabledArea>
+  //     <Runtime {...props} />
+  //   </DisabledArea>
+  // );
 };
