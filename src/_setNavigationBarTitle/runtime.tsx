831a401a08ec047fbacbf7664afd8259c6f885af
zhaoxing03 <zhaoxing03@kuaishou.com>
2025-02-28 16:43:27 +0800

    beta

--- src/_setNavigationBarTitle/runtime.tsx
+++ src/_setNavigationBarTitle/runtime.tsx
@@ -1,10 +1,11 @@
 import * as Taro from "@tarojs/taro";
+import { isH5 } from "../utils/env";
 
 export default function ({ env, data, inputs, outputs, logger }) {
   if (!env.runtime) {
     return;
   }
-  
+
   inputs["setTitle"]((value) => {
     if (value && typeof value === "string") {
       Taro.setNavigationBarTitle({
--- src/system/systemWebview/editors.tsx
+++ src/system/systemWebview/editors.tsx
@@ -1,9 +1,11 @@
+import entryPagePathEditor from "./editors/entryPagePath";
 import MybricksNavigationEditor from "./editors/mybricks-navigation";
 
 export default {
-  ':root'({ data, output, style }, cate0, cate1, cate2) {
-    cate0.title = '常规'
+  ":root"({ data, output, style }, cate0, cate1, cate2) {
+    cate0.title = "常规";
     cate0.items = [
+      { ...entryPagePathEditor },
       {
         title: "导航栏",
         items: [
@@ -41,18 +43,18 @@ export default {
               },
             },
           },
-          {
-            title: "导航栏标题文字内容",
-            type: "text",
-            value: {
-              get({ data }) {
-                return data.navigationBarTitleText;
-              },
-              set({ data }, value) {
-                data.navigationBarTitleText = value;
-              },
-            },
-          },
+          // {
+          //   title: "导航栏标题文字内容",
+          //   type: "text",
+          //   value: {
+          //     get({ data }) {
+          //       return data.navigationBarTitleText;
+          //     },
+          //     set({ data }, value) {
+          //       data.navigationBarTitleText = value;
+          //     },
+          //   },
+          // },
           {
             title:
               "在非首页、非页面栈最底层页面或非tabbar内页面中的导航栏展示home键",
@@ -72,46 +74,47 @@ export default {
         title: "",
         items: [
           {
-            title: '网页链接',
-            type: 'text',
-            description: "可打开关联的公众号的文章，其它网页需登录小程序管理后台配置业务域名。",
+            title: "网页链接",
+            type: "text",
+            description:
+              "可打开关联的公众号的文章，其它网页需登录小程序管理后台配置业务域名。",
             value: {
               get({ data }) {
-                return data.url
+                return data.url;
               },
               set({ data }, value: string) {
-                data.url = value
+                data.url = value;
               },
             },
           },
-        ]
+        ],
       },
       {
-        title: '事件',
+        title: "事件",
         items: [
           {
-            title: '网页加载成功时',
-            type: '_event',
+            title: "网页加载成功时",
+            type: "_event",
             options: {
-              outputId: 'onLoad',
+              outputId: "onLoad",
             },
           },
           {
-            title: '网页加载失败时',
-            type: '_event',
+            title: "网页加载失败时",
+            type: "_event",
             options: {
-              outputId: 'onError',
+              outputId: "onError",
             },
           },
           {
-            title: '网页向小程序 postMessage 时',
-            type: '_event',
+            title: "网页向小程序 postMessage 时",
+            type: "_event",
             options: {
-              outputId: 'onMessage',
+              outputId: "onMessage",
             },
-          }
+          },
         ],
-      }
-    ]
+      },
+    ];
   },
-}
+};
